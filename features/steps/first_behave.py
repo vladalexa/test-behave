4f1a40118e3da678a126b798a5a9acd423f78c8d
Vlad <vlad@tictrac.com>
2016-01-06 13:13:48 +0000

    from BL to dev1d

--- features/steps/first_behave.py
+++ features/steps/first_behave.py
@@ -1,4 +1,5 @@
 from behave import *
+import testlink
 import time,os
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -10,7 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 @given("I am on BL log in page")
 def step_impl(context):
 
-    context.browser.get("https://stagingbluelink.tictrac.com/py_api/api/v1/auth/saml2/test_login/vlad")
+    context.browser.get("https://staging1d.tictrac.com")
     context.browser.maximize_window()
 
 
@@ -18,21 +19,19 @@ def step_impl(context):
 
 @when("I log in")
 def step_impl(context):
-    passw=open('.\\resources\\pass.txt','r')
-    actions = ActionChains(context.browser)
-    actions.send_keys("vlad")
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(passw.readline())
-    actions.send_keys(Keys.ENTER)
-    actions.perform()
+    passw=open('C:\\Projects\\tictrac-bahave\\tictrac-qa\\tictrac-behave\\pass.txt','r')
+    context.browser.find_element_by_id('tt-form-email').send_keys('vlad@tictrac.com')
+    context.browser.find_element_by_id('tt-form-password').send_keys(passw.readline())
+    context.browser.find_element_by_xpath('/html/body/div[1]/div/div/main/div/div[1]/form[2]/div/button').click()
+    time.sleep(5)
 
-    try:
-        element = WebDriverWait(context.browser, 1).until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.ng-scope > div.page.page--with-masthead.ng-scope > div > nav > ul:nth-child(1) > li:nth-child(2) > a > div > span"))
-    )
-        element.click()
-        time.sleep(2)
-    except Exception:
-        print('error')
+    # try:
+    #     element = WebDriverWait(context.browser, 1).until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.ng-scope > div.page.page--with-masthead.ng-scope > div > nav > ul:nth-child(1) > li:nth-child(2) > a > div > span"))
+    # )
+    #     element.click()
+    #     time.sleep(2)
+    # except Exception:
+    #     print('error')
 
 
 
